181a07deff9fdf5196bb33abdd3a9162468a969e
Michael Alexander <alexandermw@gmail.com>
2014-07-29 20:59:23 -0700

    filled out a bunch of methods

---
+++ mastermind.rb
@@ -0,0 +1,126 @@
+module Mastermind
+
+class Game
+  attr_accessor codemaker, codebreaker, score
+
+  def initialize
+    @codemaker = AI.new
+    @codebreaker = Human.new
+    @board = Board.new
+    @score = 0
+  end
+
+  def start
+    get_code
+    (1..12).each {|turn| take_turn(turn)}
+  end
+
+  def get_code
+    code = codemaker.devise_code
+  end
+
+  def take_turn
+    board.display
+    guess(codebreaker)
+    answer(codemaker)
+  end
+
+  def guess(codebreaker, turn)
+    codebreaker.guess(turn)
+  end
+
+  def answer(codemaker, turn)
+    codemaker.respond(turn)
+  end
+
+end
+
+#maybe
+#class Score
+#end
+
+class Board
+
+
+  @code = Row.new
+  @guesses = []
+  @responses = []
+
+  def initialize
+  end
+
+  def set_code(code)
+    @code ||= code #test this to make sure it works
+  end
+
+  def input_guess(row)
+    @guesses << row
+  end
+
+  def input_response(row)
+    @responses << row
+  end
+
+  def this_turn
+    @guesses.size-1
+  end
+
+  def display
+    if this_turn == -1
+      puts "No guesses yet."
+    else
+      (0..this_turn).each do |turn|
+        print "Guess \##{turn+1}: "
+        print @guesses[turn]
+        print "   |  "
+        print @responses[turn]
+        puts "\n"
+      end
+    end
+  end
+end
+
+# a row represents six colors with letters A - F
+class Row
+  attr_reader
+  def initialize(code)
+    @code = code
+  end
+
+  def ==(other_thing)
+    self.code == other_thing.code
+  end
+
+  def each
+    self.code.each {yield}
+  end
+end
+
+class Response
+  attr_reader :correct, :wrong_place
+  def initialize(correct, wrong_place)
+    @correct = correct
+    @wrong_place = wrong_place
+  end
+
+end
+
+#maybe?
+#class Peg
+#end
+
+class Player
+end
+
+class AI < Player
+  def devise_code
+
+  end
+end
+
+class Human < Player
+
+end
+
+
+end
